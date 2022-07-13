098c3673dfe145fe129083141bcc88613fa36cb0
DESKTOP-IJ8C807\user1 <justlikeyejin@gmail.com>
2022-07-13 20:13:24 +0900

    로그인 기능/ 마이페이지 많이 수정 / 주문 내역 조금 수정/ 구독 내역 조금 수정/내구독현황 수정

--- Uglory_Customer/src/main/java/edu/kh/ugloryC/member/model/vo/Member.java
+++ Uglory_Customer/src/main/java/edu/kh/ugloryC/member/model/vo/Member.java
@@ -1,5 +1,21 @@
 package edu.kh.ugloryC.member.model.vo;
 
-public class Member {
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
 
+@Getter
+@Setter
+@ToString
+@NoArgsConstructor
+public class Member {
+	
+	private int memberNo;
+	private String memberName;
+	private String memberEmail;
+	private String secessionFlag;
+	private String secessionReason;
+	
+	
 }
