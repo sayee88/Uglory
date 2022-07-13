bdef27e871a2bdfc9385e88896ff869b2f101e30
KEB <12128_17@naver.com>
2022-07-13 14:19:01 +0900

    고객 조회 back-end

---
+++ Uglory_Admin/src/main/java/edu/kh/ugloryA/customer/controller/CustomerController.java
@@ -0,0 +1,37 @@
+package edu.kh.ugloryA.customer.controller;
+
+import java.util.List;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.SessionAttributes;
+
+import com.google.gson.Gson;
+
+import edu.kh.ugloryA.customer.model.service.CustomerService;
+import edu.kh.ugloryA.customer.model.vo.Customer;
+
+@Controller
+@RequestMapping("/customer")
+@SessionAttributes({"message"})
+public class CustomerController {
+
+	private Logger logger = LoggerFactory.getLogger(CustomerController.class);
+	
+	@Autowired
+	private CustomerService service;
+	
+	// 고객 목록 조회 ajax
+	@ResponseBody
+	@GetMapping("/selectAll")
+	public String selectAllCustomer() {
+		List<Customer> list = service.selectAllCustomer();
+		
+		return new Gson().toJson(list);
+	}
+}
