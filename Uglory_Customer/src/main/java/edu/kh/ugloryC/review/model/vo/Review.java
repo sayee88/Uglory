01a8d734ac45cf0abcc84f32c262ace6f2f64662
sayee <dkdbss79@naver.com>
2022-07-14 16:56:38 +0900

    VO 작성 진행중

--- Uglory_Customer/src/main/java/edu/kh/ugloryC/review/model/vo/Review.java
+++ Uglory_Customer/src/main/java/edu/kh/ugloryC/review/model/vo/Review.java
@@ -1,5 +1,12 @@
 package edu.kh.ugloryC.review.model.vo;
 
-public class Review {
+import java.sql.Date;
 
+public class Review {
+	
+	private int reviewNo;
+	private String reviewContent;
+	private Date reviewEnrollDate;
+	private int starRating;
+	private int reviewCode;
 }
