abcaf217230281e55238eec79b275cdc77a78c3a
onyourm__ark <gosodkdl@gmail.com>
2022-07-19 08:58:10 +0900

    카테고리

--- Uglory_Customer/src/main/java/edu/kh/ugloryC/subscription/controller/SubscriptionController.java
+++ Uglory_Customer/src/main/java/edu/kh/ugloryC/subscription/controller/SubscriptionController.java
@@ -19,6 +19,7 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -43,6 +44,7 @@ public class SubscriptionController {
 	public String subscription(/*@ModelAttribute("loginMember") Member loginMember,*/
 							   HttpSession session,
 							   Model model,
+							   RedirectAttributes ra,
 							   HttpServletRequest req,
 							   HttpServletResponse resp) {
 		
@@ -52,14 +54,13 @@ public class SubscriptionController {
 		
 		if(loginMember == null) {
 			path = "member/login";
+			ra.addFlashAttribute("message", "로그인 후 이용해주세요");
 			
 		}else {
 			path = "subscription/subscription1";
 		}
 		
-
-		
-		return path;
+		return "redirect:" + path;
 	}
 	
 	// 구독 페이지2
