28417417d34c1f73acfad162965a96b4b4d8a983
DESKTOP-IJ8C807\user1 <jdt1025@naver.com>
2022-07-19 21:15:40 +0900

    장바구니 진행 중

--- Uglory_Customer/src/main/java/edu/kh/ugloryC/product/controller/ProductController.java
+++ Uglory_Customer/src/main/java/edu/kh/ugloryC/product/controller/ProductController.java
@@ -1,8 +1,6 @@
 package edu.kh.ugloryC.product.controller;
 
 import java.text.SimpleDateFormat;
-import java.time.LocalDate;
-import java.time.format.DateTimeFormatter;
 import java.util.ArrayList;
 import java.util.Date;
 import java.util.HashMap;
@@ -33,12 +31,11 @@ import edu.kh.ugloryC.product.model.service.OptionService;
 import edu.kh.ugloryC.product.model.service.ProductService;
 import edu.kh.ugloryC.product.model.vo.OptionType;
 import edu.kh.ugloryC.product.model.vo.ProductDetail;
-import edu.kh.ugloryC.product.model.vo.ProductOrder;
 
 
 @Controller
 @RequestMapping("/product")
-@SessionAttributes({"loginMember"})
+@SessionAttributes({"loginMember", "cartMap"})
 public class ProductController {
 	
 	@Autowired
@@ -115,7 +112,6 @@ public class ProductController {
 //			map.put("key" , ((Double)map.get(key)).intValue() );
 //		}
 		
-		
 		// 옵션코드 리스트
 		List<String> optionCodeList = new ArrayList<String>(map.keySet());
 
@@ -141,17 +137,12 @@ public class ProductController {
 		
 		if(pOrderCode != null) {
 			
-			// 결제번호 생성
-			//LocalDate currentDate = LocalDate.now(); 
-			//String date = currentDate.format(DateTimeFormatter.ofPattern("yyMMddhh24mmss"));
-			
-			SimpleDateFormat sdf = new SimpleDateFormat("yyMMddhhmmss");
+			SimpleDateFormat sdf = new SimpleDateFormat("yyMMddHHmmss");
 			
 			String date = sdf.format(new Date());
 			
-			
 			int random = (int)(Math.random() * 5);
-			String productPayNo = "SP" + date + "-" + random;
+			String productPayNo = date + "-" + random;
 			
 			map.put("productPayNo", productPayNo);
 		}
@@ -167,7 +158,7 @@ public class ProductController {
 		return "product/productOrder";
 	}
 	
-	// 배송정보 입력
+	// 배송정보 입력 - 결제
 	@ResponseBody
 	@PostMapping("/order")
 	public int productOrder(@RequestParam(value="pOrderCode", required=false) String pOrderCode,
@@ -205,7 +196,7 @@ public class ProductController {
 
 		int result = service.productOrder(productOrder);
 		
-		if(result > 0) { // 주문 정보 입력 성공 시 결제 테이블 삽입
+		if(result > 0) { // 주문 정보 입력 및 결제 성공 시 결제 테이블 삽입
 			int productPay = service.productPay(productOrder);
 		}
 		model.addAttribute("productOrder", productOrder);
@@ -213,10 +204,49 @@ public class ProductController {
 		return result;
 	}
 	
+	// 장바구니 화면 전환
+	@SuppressWarnings({ "unchecked" })
+	@ResponseBody
 	@GetMapping("/cart")
-	public String productCart() {
+	public String cart(Integer productCode,
+			      	   Integer totalAmount, 
+			           String optionObj, Model model) {
 		
+		Map<String, Object> cartMap = new HashMap<String, Object>();
+		
+		// 넘어온 스트링을 객체 형태로 변환
+		cartMap = (Map<String, Object>)new Gson().fromJson(optionObj, cartMap.getClass());
+		
+		// 옵션코드 리스트
+		List<String> optionCodeList = new ArrayList<String>(cartMap.keySet());
+
+		// 수량 리스트
+		List<Integer> amountList = new ArrayList<Integer>();
+		for(String key : cartMap.keySet()) {
+			amountList.add( ((Double)cartMap.get(key)).intValue() );
+		}
+
+		// 	옵션코드 리스트 인덱스 == 수량 리스트 인덱스
+				
+		cartMap.clear();
+		
+		cartMap.put("optionCodeList", optionCodeList);
+		cartMap.put("amountList", amountList);
+		
+		cartMap.put("productCode", productCode);
+		
+		// 주문 페이지 내 옵션 코드 상품 코드에 따른 옵션이름, 개수 조회
+		List<OptionType> cartOptionList = service.cartOptionList(cartMap);
+		
+		cartMap.put("cartOptionList", cartOptionList);
+		cartMap.put("totalAmount", totalAmount);
+
+		return new Gson().toJson(cartMap);
+	}
+
+	@PostMapping("/cart")
+	public String productCart() {
+
 		return "product/productCart";
 	}
-	
 }
