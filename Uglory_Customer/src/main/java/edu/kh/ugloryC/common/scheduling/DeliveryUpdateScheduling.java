86ec6221c8eb231c9bf4d447eb5ab44d71383c20
onyourm__ark <gosodkdl@gmail.com>
2022-07-22 08:52:46 +0900

    🍀✨🍉

--- Uglory_Customer/src/main/java/edu/kh/ugloryC/common/scheduling/DeliveryUpdateScheduling.java
+++ Uglory_Customer/src/main/java/edu/kh/ugloryC/common/scheduling/DeliveryUpdateScheduling.java
@@ -26,7 +26,7 @@ public class DeliveryUpdateScheduling {
 	
 	
 	// 매주
-	@Scheduled(cron="0 0 15 * * 4") // 매주 수요일 3시마다
+	@Scheduled(cron="0 0 0 * * 4") // 매주 수요일 0시마다
 //	@Scheduled(cron="0 3 * * * *") // 테스트용
 	public void updateEvery() {
 		
@@ -34,7 +34,7 @@ public class DeliveryUpdateScheduling {
 		logger.info("배송중");
 	}
 	
-	@Scheduled(cron="0 0 15 * * 6") // 매주 금요일 3시마다
+	@Scheduled(cron="0 0 0 * * 6") // 매주 금요일 0시마다
 //	@Scheduled(cron="0 4 * * * *") // 테스트용
 	public void updateEveryFri() {
 		// 배송주기 '1'인 사람 중 배송중인 사람 '배송완'으로 update
@@ -46,7 +46,7 @@ public class DeliveryUpdateScheduling {
 	
 	
 	// 격주
-	@Scheduled(cron="0 0 9 * * 4#2,4#4") // 둘째 넷째 수요일 9시마다
+	@Scheduled(cron="0 0 0 * * 4#2,4#4") // 둘째 넷째 수요일 0시마다
 //	@Scheduled(cron="0 5 * * * *") // 테스트용
 	public void updateBiweekly() {
 		List<Delivery> biList = service.selectbiList();
@@ -55,7 +55,7 @@ public class DeliveryUpdateScheduling {
 	}
 	
 
-	@Scheduled(cron="0 0 15 * * 6#2,6#4") // 둘째 넷째 금요일 3시마다
+	@Scheduled(cron="0 0 0 * * 6#2,6#4") // 둘째 넷째 금요일 0시마다
 //	@Scheduled(cron="0 6 * * * *") // 테스트용
 	public void updateBiFri() {
 		// 배송주기 '2'인 사람 중 배송중인 사람 '배송완'으로 update
