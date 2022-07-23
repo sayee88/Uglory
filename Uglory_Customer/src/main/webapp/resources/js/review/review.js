6eaf78fcf8d0b4a5e0fb1a8fb8f7c7fe1e4d70ab
sayee <dkdbss79@naver.com>
2022-07-23 13:39:29 +0900

    리뷰 박스 count 수정 완료! 리뷰작성 진짜진짜 가능합니다!! (리뷰 작성 css 수정 필요)

--- Uglory_Customer/src/main/webapp/resources/js/review/review.js
+++ Uglory_Customer/src/main/webapp/resources/js/review/review.js
@@ -33,3 +33,71 @@ document.getElementsByClassName("product")[0].addEventListener("click", function
     this.classList.add("unWritten");
    
 });
+
+
+
+
+// ajax
+function subBox(){
+
+    $.ajax({
+
+        url: "selectSub",
+        type : "GET",
+        dataType : "JSON",
+        success : function(subList){
+                        // 담아올 바구니
+            // list 자체를 가져와야함
+
+
+        },
+        error: function(req, status, error){
+            console.log("리스트 조회 중 에러가 발생...");
+            console.log(req.responseText);
+        }
+    });
+
+
+
+
+
+}
+
+const reviewList = document.getElementById("review-list");
+
+function createBox(){
+
+    const reviewContent = document.createElement("div");
+    reviewContent.classList.add("review-content");
+
+    const clickModal = document.createElement("div");
+    clickModal.setAttribute("data-bs-toggle", "modal");
+    clickModal.setAttribute("data-bs-target", "#exampleModal");
+
+    const reviewImage = document.createElement("img");
+    reviewImage.setAttribute("src", contextPath + '이미지루트');
+
+    const text = document.createElement("div");
+    text.classList.add("review-content-text");
+
+    const contentTop = document.createElement("div");
+    contentTop.classList.add("review-content-top-section");
+
+    const cotentTopLeft = document.createElement("div");
+    cotentTopLeft.classList.add("review-content-top-left");
+
+    const name = document.createElement("div");
+    name.innerText = "이름 얻어오기";
+
+    const enrollDate = document.createElement("div");
+    enrollDate.innerText = "리뷰 작성일";
+
+    const starRating = document.createElement("div");
+    starRating.classList.add("review-star");
+
+    const star = document.createElement("span");
+    star.innerText = "별점";
+
+
+
+};
