753b2a688cb2bc7ce9bc5a90441803376b7faccd
KEB <12128_17@naver.com>
2022-07-24 18:59:23 +0900

    🧸 매출관리 css 일부 수정

--- Uglory_Admin/src/main/webapp/resources/js/salesList.js
+++ Uglory_Admin/src/main/webapp/resources/js/salesList.js
@@ -14,7 +14,9 @@ for(const refundBtn of refundBtnList){
             success : function(result){
                 if(result > 0){
                     alert("환불 요청 처리 완료");
+                    e.target.parentElement.innerText = "환불 처리 완료";
                     e.target.remove();
+
 
                 } else{
                     alert("환불 처리 실패");
