3e5fc0e80ec2e5f4c56490ff6141a3e8792b22f4
sayee <dkdbss79@naver.com>
2022-07-26 22:36:43 +0900

    게시글 삭제 완료,,,?

--- Uglory_Customer/src/main/webapp/resources/js/review/review.js
+++ Uglory_Customer/src/main/webapp/resources/js/review/review.js
@@ -545,6 +545,23 @@ function openModal(reviewNo){
 }
 
 
+//==========================================================
+
+
+// 나의 리뷰 js 부분
+// 리뷰 삭제
+function deleteReview(reviewNo, reviewCode){
+
+    let url = contextPath + "/review/list/delete/" + reviewCode + "/" + reviewNo;
+
+    if( confirm("정말로 삭제 하시겠습니까?")){
+        location.href = url; // get 방식으로 url에 요청
+    }
+
+}
+
+
+
 
 
 
@@ -592,8 +609,3 @@ document.getElementsByClassName("product")[0].addEventListener("click", function
     this.classList.add("unWritten");
    
 });
-
-
-
-// br 태그 제거
-text = text.replace(/<(\/)?([a-zA-Z]*)(\s[a-zA-Z]*=[^>]*)?(\s)*(\/)?>/ig, "");
