26387c9aba5e99bbd9ad431afc3040d568cdcd8e
BeLimeSH <shee5212@naver.com>
2022-07-29 12:26:57 +0900

    농장 페이지네이션

--- Uglory_Admin/src/main/java/edu/kh/ugloryA/farm/model/dao/FarmDAO.java
+++ Uglory_Admin/src/main/java/edu/kh/ugloryA/farm/model/dao/FarmDAO.java
@@ -2,10 +2,12 @@ package edu.kh.ugloryA.farm.model.dao;
 
 import java.util.List;
 
+import org.apache.ibatis.session.RowBounds;
 import org.mybatis.spring.SqlSessionTemplate;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 
+import edu.kh.ugloryA.customer.model.vo.Pagination;
 import edu.kh.ugloryA.farm.model.vo.Farm;
 
 @Repository
@@ -16,10 +18,16 @@ public class FarmDAO {
 
 	/**
 	 * 농장 리스트 조회 DAO
+	 * @param pagination 
 	 * @return farmList
 	 */
-	public List<Farm> selectFarmList() {
-		return sqlSession.selectList("farmMapper.seletFarmList");
+	public List<Farm> selectFarmList(Pagination pagination) {
+		
+		int offset = (pagination.getCurrentPage()-1) * pagination.getLimit();
+		
+		RowBounds rowbounds = new RowBounds(offset, pagination.getLimit());
+		
+		return sqlSession.selectList("farmMapper.seletFarmList", null, rowbounds);
 	}
 
 	/**
@@ -56,6 +64,14 @@ public class FarmDAO {
 	 */
 	public int insertFarm(Farm farm) {
 		return sqlSession.insert("farmMapper.insertFarm", farm);
+	}
+
+	/**
+	 * 농장 수 세기
+	 * @return
+	 */
+	public int countFarmNo() {
+		return sqlSession.selectOne("farmMapper.countFarmNo");
 	}
 
 }
